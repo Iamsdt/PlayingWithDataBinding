0c8f40588fd22e9068da3199c0f842531494e2db
Shudipto <Shudiptotrafder@gmail.com>
2019-05-26 11:40:13 +0600

    binding adapter
    Took 22 minutes

--- app/src/main/java/com/iamsdt/playingwithdatabinding/BindingAdapterActivity.kt
+++ app/src/main/java/com/iamsdt/playingwithdatabinding/BindingAdapterActivity.kt
@@ -1,9 +1,13 @@
 package com.iamsdt.playingwithdatabinding
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.constraintlayout.widget.Placeholder
 import androidx.databinding.BindingAdapter
+import androidx.databinding.BindingMethod
+import androidx.databinding.BindingMethods
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
 import com.iamsdt.playingwithdatabinding.databinding.ActivityBindingAdapterBinding
@@ -16,7 +20,7 @@ class BindingAdapterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_binding_adapter)
-
+        binding.lifecycleOwner = this
         binding.url =
             MyDataClass(
                 name = "https://images.unsplash.com/photo-1517404215738-15263e9f9178?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
@@ -25,12 +29,26 @@ class BindingAdapterActivity : AppCompatActivity() {
     }
 }
 
-//must declare as top level function
-@BindingAdapter("loadImage")
-fun loadImage(view: ImageView, url: String) {
+@BindingAdapter(value = ["loadImage", "placeHolder"], requireAll = true)
+fun loadImage(view: ImageView, url: String?, placeholder: Drawable) {
     Timber.i("######## $url############")
-    Glide.with(view.context)
-        .load(url)
-        .fitCenter()
-        .into(view)
+    if (url != null && url.isNotEmpty()) {
+        Glide.with(view.context)
+            .load(url)
+            .placeholder(placeholder)
+            .fitCenter()
+            .into(view)
+    } else {
+        view.setImageDrawable(placeholder)
+    }
 }
+
+//must declare as top level function
+//@BindingAdapter("loadImage")
+//fun loadImage(view: ImageView, url: String) {
+//    Timber.i("######## $url############")
+//    Glide.with(view.context)
+//        .load(url)
+//        .fitCenter()
+//        .into(view)
+//}
