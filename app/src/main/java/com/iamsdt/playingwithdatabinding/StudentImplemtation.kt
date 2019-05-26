67e97d4aad64a4b7c7c7714e290c30a3512a4488
Shudipto <Shudiptotrafder@gmail.com>
2019-05-26 13:47:02 +0600

    Observe object
    Took 9 minutes

---
+++ app/src/main/java/com/iamsdt/playingwithdatabinding/StudentImplemtation.kt
@@ -0,0 +1,29 @@
+package com.iamsdt.playingwithdatabinding
+
+import androidx.databinding.Bindable
+import androidx.databinding.Observable
+import androidx.databinding.PropertyChangeRegistry
+
+class StudentImplemtation(var _title: String) : Observable {
+
+    private var registry: PropertyChangeRegistry = PropertyChangeRegistry()
+
+    var title: String
+        @Bindable
+        get() = _title
+        set(value) {
+            _title = value
+            //use in xml
+            registry.notifyChange(this, BR.title)
+        }
+
+    override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+        registry.add(callback)
+    }
+
+    override fun addOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+        registry.remove(callback)
+    }
+
+
+}
