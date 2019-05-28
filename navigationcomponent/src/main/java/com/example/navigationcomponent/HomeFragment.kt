047a0e938792e78fe00c21bed511eb815a9763b9
Iamsdt <Shudiptotrafder@gmail.com>
2019-05-28 22:54:48 +0600

    navigation
    Took 45 minutes

---
+++ navigationcomponent/src/main/java/com/example/navigationcomponent/HomeFragment.kt
@@ -0,0 +1,26 @@
+package com.example.navigationcomponent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_home.view.*
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        view.button.setOnClickListener {
+            view.findNavController().navigate(R.id.action_homeFragment_to_aboutFragment)
+        }
+
+        return view
+    }
+}
